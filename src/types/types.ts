2ffaf9fbc43cec8e4bde647cae0486f6edfb635a
Passam <kuba.myszczyszyn@gmail.com>
2024-10-31 22:34:30 +0100

    Add TypeScript interface for Washing Machines

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export interface WashingMachine {
+    id: number;
+    name: string;
+    capacity: string;
+    dimensions: string;
+    functions: string[];
+    energyClass: string;
+    price: number;
+    imageUrl: string;
+}
