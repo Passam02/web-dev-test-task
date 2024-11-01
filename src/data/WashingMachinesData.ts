7c539008263086346877385d9aceb09197759909
Passam <kuba.myszczyszyn@gmail.com>
2024-11-02 00:04:26 +0100

    Add Washing Machines Data

---
+++ src/data/WashingMachinesData.ts
@@ -0,0 +1,71 @@
+// src/data/washingMachinesData.ts
+const washingMachinesData = [
+    {
+        id: 1,
+        name: "WW90T754ABT, Pralka QuickDrive™, 9 kg, biała",
+        capacity: '9',
+        dimensions: "55 x 60 x 85 cm",
+        functions: ["Drzwi AddWash™", "Panel AI Control", "Silnik inwerterowy", "Wyświetlacz elektroniczny"],
+        energyClass: "A",
+        price: 3199,
+        imageUrl: "https://s3-alpha-sig.figma.com/img/4e08/a31e/8b634cbc6575c09aa52521b676fe70fc?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aw54s4Xe1qlAvitP2zMeAwy5tL-FpiUTPQxglsBn4w5tQ4wjWTRB8IZcdqpNiFgZdh9jIKvRt4JlKy1-77yzajIx52IuStZFoFImUkCeQ6ionvjrGQOYUpjURLObpubYzwSWzO2UbS5H~ADGzZfOzdhujr1QlKpnqGZZjoBFvWq4qr793Km2Ptm1zfR0RjSjiVvjUfF2Bsmoapymo7vMryDSAt05W8q~LM7SiqKrjXH7xE1PgcX84CL6aV3cnuIErcmpPf91oDywPqaFVf-qj9NUoz4R1gVRdvbas3oZLEVUEJZ0Py5oynaunqJiK2sqerCmOyiv10QxphaSkJ7l7g__",
+        instalmentsPlan: true
+    },
+    {
+        id: 2,
+        name: "WW10T654DLH, Pralka EcoBubble™, 10,5 kg, biała",
+        capacity: '10,5',
+        dimensions: "55 x 60 x 85 cm",
+        functions: ["Drzwi AddWash™", "Panel AI Control", "Silnik inwerterowy", "Wyświetlacz elektroniczny"],
+        energyClass: "B",
+        price: 3199,
+        imageUrl: "https://s3-alpha-sig.figma.com/img/2f77/5bf7/29788817dcdbb04bb6f304b138151d4a?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VXBC6PkaDhVSer0~uNz6ChJVU6ilFAORERw7wSyFhKKPrBFxl0-fJYgLV3jeiYRDt~lGe4div~XxTfuMp3PDcASieJ899gHPP73PFNuFulAFHLsw~jxjVylLKueKzPKrZCu07eilxmlFhcAK37m3zD81vnGcc5cyX-4gd~j07p-UpvOJ-IPElBDB-DM6Y1I72h6Gx~c0DU~MvhS82318U5tKt-kWPZ5Hiihk2sw9uxtJiE0VT5gwNonDsyon~uy2JL~ak7ifnEDPkl1WRgq7CSmfyxqwbTV8ZrAxR5qrz6LFO0Wikjb8gY0yyKx-QJ-dcQA2T4nq2YU-FX6yNRsgiQ__",
+        instalmentsPlan: true
+    },
+    {
+        id: 3,
+        name: "WW90T654DLH, Pralka EcoBubble™, 8 kg, biała",
+        capacity: '8',
+        dimensions: "55 x 60 x 85 cm",
+        functions: ["Drzwi AddWash™", "Panel AI Control", "Silnik inwerterowy", "Wyświetlacz elektroniczny"],
+        energyClass: "C",
+        price: 3199,
+        imageUrl: "https://s3-alpha-sig.figma.com/img/5cd2/ccb5/61cd583d1f73095b58fde18e578a6229?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=od5clQ2HdLo47y4tviF9TKEMU7itDmU0ThjHXlJHeVsvbmyvC-ufF5o9KsmPZH5U9SvKCEdL-XW78hqdB88Srrg7D4kynm21bQ6it2cBrBCTy1rUgFLJwpUPdfxesMqrgSWT414XWuOSqSpDv1qgq0Weh9H4wfAwBoCvasuEcHJ1mhn9CE5Mly1v3u5aJZV-xaufEi1dFLDosCN4NubNUeWUPdLSEVZ8d0hQesxgyQi~slfaiKVPkcZAkAcXNPDW-oE9Xbey6pnExelinL8ThRLFEdCstRCJbI0qliGn3zicRhz2aemYdZgr7kYBt2rxBiafFSK0kTHf4EGka3Rj6Q__",
+        instalmentsPlan: true
+    },
+    {
+        id: 4,
+        name: "WW90T754ABT, Pralka QuickDrive™, 9 kg, biała",
+        capacity: '9',
+        dimensions: "55 x 60 x 85 cm",
+        functions: ["Drzwi AddWash™", "Panel AI Control", "Silnik inwerterowy", "Wyświetlacz elektroniczny"],
+        energyClass: "D",
+        price: 3199,
+        imageUrl: "https://s3-alpha-sig.figma.com/img/4e08/a31e/8b634cbc6575c09aa52521b676fe70fc?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aw54s4Xe1qlAvitP2zMeAwy5tL-FpiUTPQxglsBn4w5tQ4wjWTRB8IZcdqpNiFgZdh9jIKvRt4JlKy1-77yzajIx52IuStZFoFImUkCeQ6ionvjrGQOYUpjURLObpubYzwSWzO2UbS5H~ADGzZfOzdhujr1QlKpnqGZZjoBFvWq4qr793Km2Ptm1zfR0RjSjiVvjUfF2Bsmoapymo7vMryDSAt05W8q~LM7SiqKrjXH7xE1PgcX84CL6aV3cnuIErcmpPf91oDywPqaFVf-qj9NUoz4R1gVRdvbas3oZLEVUEJZ0Py5oynaunqJiK2sqerCmOyiv10QxphaSkJ7l7g__",
+        instalmentsPlan: false
+    },
+    {
+        id: 5,
+        name: "WW10T654DLH, Pralka EcoBubble™, 10,5 kg, biała",
+        capacity: '10,5',
+        dimensions: "55 x 60 x 85 cm",
+        functions: ["Drzwi AddWash™", "Panel AI Control", "Silnik inwerterowy", "Wyświetlacz elektroniczny"],
+        energyClass: "E",
+        price: 3199,
+        imageUrl: "https://s3-alpha-sig.figma.com/img/2f77/5bf7/29788817dcdbb04bb6f304b138151d4a?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VXBC6PkaDhVSer0~uNz6ChJVU6ilFAORERw7wSyFhKKPrBFxl0-fJYgLV3jeiYRDt~lGe4div~XxTfuMp3PDcASieJ899gHPP73PFNuFulAFHLsw~jxjVylLKueKzPKrZCu07eilxmlFhcAK37m3zD81vnGcc5cyX-4gd~j07p-UpvOJ-IPElBDB-DM6Y1I72h6Gx~c0DU~MvhS82318U5tKt-kWPZ5Hiihk2sw9uxtJiE0VT5gwNonDsyon~uy2JL~ak7ifnEDPkl1WRgq7CSmfyxqwbTV8ZrAxR5qrz6LFO0Wikjb8gY0yyKx-QJ-dcQA2T4nq2YU-FX6yNRsgiQ__",
+        instalmentsPlan: false
+    },
+    {
+        id: 6,
+        name: "WW90T654DLH, Pralka EcoBubble™, 8 kg, biała",
+        capacity: '8',
+        dimensions: "55 x 60 x 85 cm",
+        functions: ["Drzwi AddWash™", "Panel AI Control", "Silnik inwerterowy", "Wyświetlacz elektroniczny"],
+        energyClass: "F",
+        price: 3199,
+        imageUrl: "https://s3-alpha-sig.figma.com/img/5cd2/ccb5/61cd583d1f73095b58fde18e578a6229?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=od5clQ2HdLo47y4tviF9TKEMU7itDmU0ThjHXlJHeVsvbmyvC-ufF5o9KsmPZH5U9SvKCEdL-XW78hqdB88Srrg7D4kynm21bQ6it2cBrBCTy1rUgFLJwpUPdfxesMqrgSWT414XWuOSqSpDv1qgq0Weh9H4wfAwBoCvasuEcHJ1mhn9CE5Mly1v3u5aJZV-xaufEi1dFLDosCN4NubNUeWUPdLSEVZ8d0hQesxgyQi~slfaiKVPkcZAkAcXNPDW-oE9Xbey6pnExelinL8ThRLFEdCstRCJbI0qliGn3zicRhz2aemYdZgr7kYBt2rxBiafFSK0kTHf4EGka3Rj6Q__",
+        instalmentsPlan: false
+    },
+  ];
+  
+  export default washingMachinesData;
